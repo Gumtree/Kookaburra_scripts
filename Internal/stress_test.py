f0b9e7bad817fba810532a68b048fe40cb7dd359
nxi <nxi@NXI-DEV1.nbi.ansto.gov.au>
2021-02-08 10:24:42 +1100

    finish stree_test.py

--- Internal/stress_test.py
+++ Internal/stress_test.py
@@ -1,7 +1,27 @@
+from org.gumtree.gumnix.sics.io import SicsExecutionException
+'''
+ Below is a script to test driving a motor to a random value in 
+ certain range for a defined number of times. 
+ 
+ Parameters:
+     motor_name : name of the motor to be tested
+     
+     motor_limits : the lower and upper limits of the motor range
+     
+     no_of_runs : number of runs in the test
+     
+     sleep_between_runs : wait for certain time between each run
+     
+''' 
 motor_name = 'samz'
 motor_limits = [28, 630]
 no_of_runs = 100
 sleep_between_runs = 5
+
+# below parameters for debugging purpose, please don't change them
+#motor_name = 'dummy_motor'
+#motor_limits = [0, 100]
+#no_of_runs = 5
 
 def run():
     s = 0
@@ -14,10 +34,14 @@ def run():
             sics.drive(motor_name, pos[i])
             slog('successful')
             s += 1
+        except SicsExecutionException, ex:
+            slog('interrupted')
+            slog('The test is interrupted with {} success and {} fails'.format(s, f))
+            raise
         except:
             slog('failed')
             f += 1
         time.sleep(sleep_between_runs)
     slog('The test is finished with {} success and {} fails'.format(s, f))
         
-    
+run()
